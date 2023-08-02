4d0a2147ac8d52169d42fccd90da864dd9dfbea2
omarjabr <omarjabr165@gmail.com>
2023-08-02 12:35:32 +0300

    FIRST COMMIT

---
+++ app/layout.jsx
@@ -0,0 +1,27 @@
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "A place to find prompts for your next project.",
+};
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
